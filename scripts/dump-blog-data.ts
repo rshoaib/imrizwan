84ea2872f34a4be6d3c5f6b9276c0a95f68d1be2
Rizwan <sh_rizwan44@hotmail.com>
2026-03-22 04:31:58 +0300

    chore: remove 19 unused files (logs, temp scripts, SQL, data dumps)

--- scripts/dump-blog-data.ts
+++
@@ -1,7 +0,0 @@
-import { blogPosts } from '../data/blog';
-import * as fs from 'fs';
-import * as path from 'path';
-
-const outputPath = path.resolve(__dirname, 'temp-blog-data.json');
-fs.writeFileSync(outputPath, JSON.stringify(blogPosts, null, 2));
-console.log('Successfully dumped blogPosts to temp-blog-data.json');
